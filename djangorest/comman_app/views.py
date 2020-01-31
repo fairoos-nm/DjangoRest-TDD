bab8f74917af230da0215d6942d845f33b21f6de
muhammed fairoos <muhammed.fairoos@tarams.com>
2020-01-31 17:56:19 +0530

    GREEN : Adds views for PUT, GET and DELET also adds urls for them

--- djangorest/comman_app/views.py
+++ djangorest/comman_app/views.py
@@ -11,3 +11,11 @@ class CreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         """Save the post data when creating a new bucketlist."""
         serializer.save()
+
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    This class handles the http GET, PUT and DELETE request
+    """
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
