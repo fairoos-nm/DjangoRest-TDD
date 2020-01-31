6de5f51e0d9b47b32a12ff5a0a7f5d8b659f3029
muhammed fairoos <muhammed.fairoos@tarams.com>
2020-01-31 11:55:09 +0530

    RED: Adds test case for bucklist model

--- djangorest/comman_app/models.py
+++ djangorest/comman_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Bucketlist(models.Model):
+    """
+    This class defines the bucklist model.
+    """
+    pass
--- djangorest/comman_app/tests.py
+++ djangorest/comman_app/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Comments
 
-# Create your tests here.
+
+class ModelTestCase(TestCase):
+    """
+    This class defines the test suite for the Bucketlist model.
+    """
+
+    def setUp(self):
+        """
+        Define test client and other client varibale
+        """
+        self.bucklist_name = 'Write world class code'
+        self.bucklist = Bucketlist(name=self.bucklist_name)
+
+    def test_model_can_create_bucketlist(self):
+        """
+        Test bucklist model can create a bucklist
+        """
+        old_count = Bucketlist.objects.count()
+        self.bucklist.save()
+        new_count = Bucketlist.objects.count()
+        self.assertNotEqual(old_count, new_count)
