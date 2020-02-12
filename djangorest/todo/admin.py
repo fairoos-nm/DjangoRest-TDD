23fc37c44acc3c042fd8a30797aec3932e38fe62
muhammed fairoos <muhammed.fairoos@tarams.com>
2020-02-12 12:02:34 +0530

    register todo model in admin

--- djangorest/todo/admin.py
+++ djangorest/todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    """
+    This class define the admin config for the model todo.
+    """
+    list_display = ('title', 'description', 'completed')
+
+#Model registration
+admin.site.register(Todo, TodoAdmin)
