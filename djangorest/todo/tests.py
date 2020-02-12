fb5c600c04acd67cd371246b02e8cd4fe2b42744
muhammed fairoos <muhammed.fairoos@tarams.com>
2020-02-12 11:48:02 +0530

    RED : Adds test to check the Todo model can create ToDo list

--- djangorest/todo/tests.py
+++ djangorest/todo/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+
+
+
+
+class ModelTestCase(TestCase):
+    """
+    This class defines the test suite for the Todo model.
+    """
+
+    def setUp(self):
+        """
+        Define the client and other varibale
+        """
+
+        self.title = 'Plan 2020!'
+        self.description = 'Go to Lendon'
+        self.todo = Todo(title=self.title,
+                         description=self.description)
+
+    def test_model_can_create_todo(self):
+        """
+        Test todo model can create a todo list.
+        """
+        old_count = Todo.objects.count()
+        self.todo.save()
+        new_count = Todo.objects.count()
+        self.assertNotEqual(old_count, new_count)
+        
